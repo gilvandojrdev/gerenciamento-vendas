1fee6ab50a5f634155127d33ec0329dd2982ccfe
Francisco Gilvando <juniorgilvando16@gmail.com>
2026-05-24 10:03:26 -0300

    Adicionando o modificador public

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.math.BigDecimal;
 
 class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
         ClienteController controller = new ClienteController();
