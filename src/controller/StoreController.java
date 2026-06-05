d1794cca86994a29e4e26d93d0a3d3af3fb5199c
gilvando <juniorgilvando16@gmail.com>
2026-06-05 16:54:20 -0300

    Adição da Injenção de dependência e inversão de controle mais a criação do método registrar produto.

--- src/controller/StoreController.java
+++ src/controller/StoreController.java
@@ -1,14 +1,30 @@
 package controller;
 
+import interfaces.ProductRepository;
+import model.Product;
+import repository.MemoryProductRepository;
+
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class StoreController {
+
+    private final ProductRepository repository;
+
+    public StoreController(ProductRepository repository) {
+        this.repository = repository;
+    }
 
     public boolean login(String user, String password){
         if(Objects.equals(user, "admin") && Objects.equals(password, "admin")){
             return true;
         }
         return false;
+    }
+
+    public void registerProduct(String prodName, String prodDescription, String prodCharacteristics, BigDecimal prodPrice, int prodStock){
+        Product product = new Product(prodName, prodDescription, prodCharacteristics, prodPrice, prodStock);
+        repository.save(product);
     }
 
 }
