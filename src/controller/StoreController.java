9dd09e629870d4a42fb76da77460cd2271f4ced2
gilvando <juniorgilvando16@gmail.com>
2026-06-05 21:16:25 -0300

    Implementação dos metódos criar, remover, visualizar e filtrar os produtos

--- src/controller/StoreController.java
+++ src/controller/StoreController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import exceptions.CategoryNotFound;
+import exceptions.ProductNotFound;
 import interfaces.ProductRepository;
+import model.Customer;
 import model.Product;
 import repository.MemoryProductRepository;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 public class StoreController {
@@ -22,9 +26,59 @@ public class StoreController {
         return false;
     }
 
-    public void registerProduct(String prodName, String prodDescription, String prodCharacteristics, BigDecimal prodPrice, int prodStock){
-        Product product = new Product(prodName, prodDescription, prodCharacteristics, prodPrice, prodStock);
+    public void registerProduct(String prodCategory, String prodName, String prodDescription, String prodCharacteristics, BigDecimal prodPrice, int prodStock){
+        Product product = new Product(prodCategory, prodName, prodDescription, prodCharacteristics, prodPrice, prodStock);
         repository.save(product);
     }
 
+    public void removerProduct(int productId) throws ProductNotFound {
+        boolean exclusion = repository.remove(productId);
+
+        if(!exclusion){
+            throw new ProductNotFound("Produto de ID: " + productId + " não foi encontrado.");
+        }
+
+        System.out.println("Usuário do ID:" + productId + " removido com sucesso");
+
+    }
+
+    public void viewProducts(){
+        List<Product> products = repository.view();
+
+        if(products.isEmpty()){
+            System.out.println("Não existe produtos");
+        }
+
+        System.out.println("Clientes cadastrados: \n");
+
+        for (Product p : products) {
+            System.out.println("ID: " + p.getProdId());
+            System.out.println("Catégoria: " + p.getProdCategory());
+            System.out.println("Nome: " + p.getProdName());
+            System.out.println("Descrição: " + p.getProdDescription());
+            System.out.println("Características: " + p.getProdCharacteristics());
+            System.out.println("Preço: " + p.getProdPrice());
+            System.out.println("-------------------------");
+        }
+    }
+
+    public void filterProducts(String category) throws CategoryNotFound {
+        List<Product> filteredProducts = repository.filter(category);
+
+        if(filteredProducts.isEmpty()){
+           throw new CategoryNotFound("Não existe produtos com a categória: " + category);
+        }
+
+        System.out.println("Produtos com a cateǵoria" + category + " :");
+
+        for (Product p : filteredProducts){
+            System.out.println("ID: " + p.getProdId());
+            System.out.println("Catégoria: " + p.getProdCategory());
+            System.out.println("Nome: " + p.getProdName());
+            System.out.println("Descrição: " + p.getProdDescription());
+            System.out.println("Características: " + p.getProdCharacteristics());
+            System.out.println("Preço: " + p.getProdPrice());
+            System.out.println("-------------------------");
+        }
+    }
 }
